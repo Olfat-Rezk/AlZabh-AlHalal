4f9a1fe66cc00a74de268e1a7de528d79c8618d6
Olfat-Rezk <Olfatsaad66@gmail.com>
2023-03-01 12:11:39 +0200

    category crud

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\categoryContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/category',[categoryContoller::class,'index']);
+Route::post('/category/store',[categoryContoller::class,'store']);
